feb717bda09c5a70524b2b4f3a070690c403b8a4
pierr05 <pierreachlet322@gmail.com>
2023-04-18 10:29:56 -0400

    Added code to reaction schema

---
+++ models/Reaction.js
@@ -0,0 +1,38 @@
+const {schema, Types, Schema} = require('mongoose'); 
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+          type: Schema.Types.ObjectId,
+          default: () => new Types.ObjectId()
+        },
+
+        reactionBody: {
+            type: String,
+            required: true,
+            max_length: 280
+        },
+
+        username: {
+            type: String,
+            required: true
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+            // timestamps need to be added
+            get: (timestamps) => dateFormat(timestamps)
+        }
+    },
+
+    {
+        timestamps: true,
+        toJSON: {
+            getters: true
+        },
+        id: false
+    }
+); 
+
+module.exports = reactionSchema; 
