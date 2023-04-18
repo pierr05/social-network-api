e9616d1f244cc970399c411f7a3df77d029a26c7
pierr05 <pierreachlet322@gmail.com>
2023-04-18 10:30:20 -0400

    Added code to thought model

---
+++ models/Thought.js
@@ -0,0 +1,43 @@
+const {Schema, model} = require('mongoose');
+const reactionSchema = require('./Reaction'); 
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            max_length: 1-280,
+        },
+
+        username: {
+            type: String,
+            required: true
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+            //timestamps need to be added
+            get: (date) => timeSince(date) 
+        }, 
+        
+        reactions: [reactionSchema],
+
+    },
+
+    {
+        timestamps: true,
+        toJSON: {
+            getters: true
+        }
+    }
+); 
+
+thoughtSchema
+.virtual('reactionCount')
+.get(function() {
+    return this.reactions.length; 
+}); 
+
+const Thought = model('thought', thoughtSchema);
+module.exports = Thought; 
