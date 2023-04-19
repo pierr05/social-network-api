015206a7fae40945ee831a6a8ceb4109a8cb69f1
pierr05 <pierreachlet322@gmail.com>
2023-04-19 10:58:59 -0400

    Added code to userRoutes.js file

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,25 +1,63 @@
 const router = require('express').Router();
-const { User, Thought } = require('/models');
+const { User} = require('../../models');
 
 // get all users
-router.get('/api/users', (req, res) => {
-    User.find({}, (err, result) => {
+router.get('/', (req, res) => {
+    User.find({})
+    .then((result) => {
         if(result) {
             res.status(200).json(result);
-
+    
         } else {
             res.status(500).json({ message: 'something went wrong' });
         }
-    });
+    })
 });
 
 // get single user by _id
-router.get('/api/users/:_id', (req, res) => {
-    User.findOne({_id: req.params.id}).populate('thoughts').populate('friends')
-        .then((user) => 
+router.get('/:id', (req, res) => {
+    User.findById(req.params.id).populate('thoughts').populate('friends')
+        .then((user) => {
             !user ? res.status(404).json({ message: 'No user with that ID' }) : res.json(user)
-        )
-        .catch((err) => res.status(500).json(err))
+        })
+        .catch((err) => {
+            res.status(500).json(err)
+        })
+})
+
+// post new user
+router.post('/', (req, res) => {
+    User.create(req.body)
+        .then((user) => {
+            res.json(user)
+        })
+        .catch((err) => {
+            res.status(500).json(err)
+        })
+}); 
+
+// update user by it's id 
+router.put('/:userId', (req, res) => {
+    User.findOneAndUpdate(
+        {_id: req.params.userId},
+        { $set: req.body },
+        { runValidators: true, new: true }
+    )
+        .then((user) => {
+            !user ? res.status(404).json({ message: 'No user found with this id!' })
+            : res.json(user)
+        })
+        .catch((err) => {
+            res.status(500).json(err)
+        })
+})
+
+// delete a user by it's id 
+router.delete('/:userId', (req, res) => {
+    User.findOneAndDelete({_id: req.params.userId})
+        .then((user) => {
+            !user ? res.status(404).json({ message: 'No user found with this id'}) : res.status(200).json({ message: 'User deleted'})
+        })
 })
 
 module.exports = router;
