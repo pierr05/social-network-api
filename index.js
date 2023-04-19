6e418816541ba87c12f1c9257e036cfff7bbaf90
pierr05 <pierreachlet322@gmail.com>
2023-04-19 10:53:52 -0400

    Added code to index.js file

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 
 const express = require('express');
 const db = require('./config/connection');
+const routes = require('./routes/api');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json()); 
+app.use('/api', routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
