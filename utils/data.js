cedb79bf4df5a146d7f039fa4ab4113816be6deb
pierr05 <pierreachlet322@gmail.com>
2023-04-19 10:59:34 -0400

    Added code to data.js file in utils folder

---
+++ utils/data.js
@@ -0,0 +1,23 @@
+const userData = [
+    {
+        username: "achlet322",
+        useremail: "pierreachlert322@gmail.com"
+    },
+    {
+        username: "casabona",
+        useremail: "casabona@gmail.com"
+    }
+];
+
+const thoughtData = [
+    {
+        thoughtText: "Here's a cool thought...",
+        username: "lernantino"
+    },
+    {
+        thoughtText: "Cool idea",
+        username: "coder123"
+    }
+];
+
+module.exports = {userData, thoughtData}; 
